01a648bc3ab1bd65f5f042d6a549577388c0930c
southli <961065551@qq.com>
2021-10-29 17:32:37 +0800

    study grpc

---
+++ go.mod
@@ -0,0 +1,19 @@
+module tag-service
+
+go 1.17
+
+require (
+	github.com/elazarl/go-bindata-assetfs v1.0.1
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309
+	google.golang.org/genproto v0.0.0-20211028162531-8db9c33dc351
+	google.golang.org/grpc v1.41.0
+)
+
+require (
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+	"log"
+	"net/http"
+	"path"
+	"strings"
+	"tag-service/pkg/swagger"
+	pb "tag-service/proto"
+	"tag-service/server"
+)
+
+var port string
+
+type httpError struct {
+	Code    int32  `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func grpcGatewayError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, _ *http.Request, err error) {
+	s, ok := status.FromError(err)
+	if !ok {
+		s = status.New(codes.Unknown, err.Error())
+	}
+
+	httpError := httpError{Code: int32(s.Code()), Message: s.Message()}
+	details := s.Details()
+	for _, detail := range details {
+		if v, ok := detail.(*pb.Error); ok {
+			httpError.Code = v.Code
+			httpError.Message = v.Message
+		}
+	}
+	resp, _ := json.Marshal(httpError)
+	w.Header().Set("Content-type", marshaler.ContentType())
+	w.WriteHeader(runtime.HTTPStatusFromCode(s.Code()))
+	_, _ = w.Write(resp)
+}
+
+func init() {
+	flag.StringVar(&port, "port", "8004", "动端口号")
+	flag.Parse()
+}
+
+func main() {
+	err := RunServer(port)
+	if err != nil {
+		log.Fatalf("Run Serve err : %v", err)
+	}
+}
+
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if request.ProtoMajor == 2 && strings.Contains(request.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(writer, request)
+		} else {
+			otherHandler.ServeHTTP(writer, request)
+		}
+	}), &http2.Server{})
+}
+
+func RunServer(port string) error {
+	httpMux := runHttpServer()
+	grpcS := runGrpcServer()
+	gatewayMux := runGrpcGatewayServer()
+	httpMux.Handle("/", gatewayMux)
+	return http.ListenAndServe(":"+port, grpcHandlerFunc(grpcS, httpMux))
+}
+
+func runHttpServer() *http.ServeMux {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte(`pong`))
+	})
+	prefix := "/swagger-ui/"
+	fileServer := http.FileServer(&assetfs.AssetFS{
+		Asset:    swagger.Asset,
+		AssetDir: swagger.AssetDir,
+		Prefix:   "third_party/swagger-ui",
+	})
+	serveMux.Handle(prefix, http.StripPrefix(prefix, fileServer))
+	serveMux.HandleFunc("/swagger/", func(writer http.ResponseWriter, request *http.Request) {
+		if !strings.HasSuffix(request.URL.Path, "swagger.json") {
+			http.NotFound(writer, request)
+			return
+		}
+		p := strings.TrimPrefix(request.URL.Path, "/swagger/")
+		p = path.Join("proto", p)
+		http.ServeFile(writer, request, p)
+	})
+	return serveMux
+}
+
+func runGrpcServer() *grpc.Server {
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			HelloInterceptor,
+			WorldInterceptor,
+		)),
+	}
+	s := grpc.NewServer(opts...)
+	pb.RegisterTagServiceServer(s, server.NewTagServer())
+	reflection.Register(s)
+	return s
+}
+
+func runGrpcGatewayServer() *runtime.ServeMux {
+	endpoint := "0.0.0.0" + port
+	gwmux := runtime.NewServeMux()
+	dopts := []grpc.DialOption{grpc.WithInsecure()}
+	_ = pb.RegisterTagServiceHandlerFromEndpoint(context.Background(), gwmux, endpoint, dopts)
+	return gwmux
+}
+
+func HelloInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("你好")
+	resp, err := handler(ctx, req)
+	log.Println("再见")
+	return resp, err
+}
+
+func WorldInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("你好,世界")
+	resp, err := handler(ctx, req)
+	log.Println("再见，世界")
+	return resp, err
+}
